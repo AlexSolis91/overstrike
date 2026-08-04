ab0e891fa415af040a12d0b8ccb43db4348632ca
AlexSolis91 <solisalex8291@gmail.com>
2026-08-04 00:16:13 -0600

    Fix: ancho de tarjeta del jefe fijo (calculado del video, ya no depende de badges de debuffs)

--- js/init-render.js
+++ js/init-render.js
@@ -742,7 +742,7 @@
                         // Portrait
                         '<div class="boss-portrait-wrap">' +
                         (char.portraitVideo
-                            ? '<video class="boss-portrait' + (isDefeated ? ' defeated-img' : '') + '" src="' + char.portraitVideo + '" autoplay loop muted playsinline></video>'
+                            ? '<video class="boss-portrait' + (isDefeated ? ' defeated-img' : '') + '" src="' + char.portraitVideo + '" autoplay loop muted playsinline onloadedmetadata="var _bc=this.closest(\'.boss-card\'); if(_bc && !_bc.dataset.widthLocked){ var _w=this.offsetHeight*(this.videoWidth/this.videoHeight); if(_w>0){ _bc.style.width=Math.round(_w)+\'px\'; _bc.dataset.widthLocked=\'1\'; } }"></video>'
                             : (activePortrait
                                 ? '<img class="boss-portrait' + (isDefeated ? ' defeated-img' : '') + '" src="' + activePortrait + '" alt="' + name + '" loading="eager" referrerpolicy="no-referrer">'
                                 : '<div class="boss-portrait-placeholder">👹</div>')) +
